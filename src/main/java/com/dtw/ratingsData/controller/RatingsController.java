932bad6d644a6fda777f375946c24abdc54cfeb7
David de Castro Hernandez <itsdavidch@gmail.com>
2021-04-28 22:36:47 +0200

    Changes made on tutorial up to episode 17

---
+++ src/main/java/com/dtw/ratingsData/controller/RatingsController.java
@@ -0,0 +1,35 @@
+package com.dtw.ratingsData.controller;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.dtw.ratingsData.model.Rating;
+import com.dtw.ratingsData.model.UserRating;
+
+@RestController
+@RequestMapping("/ratings")
+public class RatingsController {
+
+	@GetMapping("/{movieId}")
+	public Rating getRating(@PathVariable String movieId) {
+		
+		return new Rating(movieId, 4);
+	}
+	
+	@GetMapping("/users/{userId}")
+	public UserRating getUserRating(@PathVariable String userId) {
+		
+		List<Rating> ratings =  Arrays.asList(
+				new Rating("1234", 4),
+				new Rating("5678", 3));
+		
+		UserRating userRating = new UserRating();
+		userRating.setUserRating(ratings);
+		return userRating;
+	}
+}
