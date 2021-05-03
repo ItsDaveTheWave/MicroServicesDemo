0fb8987f879b08ef27d736e2c3a06ccb924d537c
David de Castro Hernandez <itsdavidch@gmail.com>
2021-05-03 23:25:12 +0200

    Customized microservices from the tutorial to my own style

--- src/main/java/com/dtw/movieCatalog/controller/MovieCatalogController.java
+++ src/main/java/com/dtw/movieCatalog/controller/MovieCatalogController.java
@@ -1,12 +1,11 @@
 package com.dtw.movieCatalog.controller;
 
-import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.reactive.function.client.WebClient;
@@ -14,7 +13,6 @@ import org.springframework.web.reactive.function.client.WebClient;
 import com.dtw.movieCatalog.model.CatalogItem;
 import com.dtw.movieCatalog.model.Movie;
 import com.dtw.movieCatalog.model.Rating;
-import com.dtw.movieCatalog.model.UserRating;
 
 @RestController
 @RequestMapping("/catalog")
@@ -23,24 +21,24 @@ public class MovieCatalogController {
 	@Autowired
 	private WebClient.Builder webClientBuilder;
 	
-	@GetMapping("/{userId}")
-	public List<CatalogItem> getCatalog(@PathVariable String userId) {
+	@GetMapping
+	public List<CatalogItem> getCatalog() {
 		
-		UserRating userRating = webClientBuilder.build()
+		List<Rating> ratingList = webClientBuilder.build()
 				.get()
-				.uri("http://localhost:8083/ratings/users/" + userId)
+				.uri("http://localhost:8083/rating")
 				.retrieve()
-				.bodyToMono(UserRating.class)
+				.bodyToMono(new ParameterizedTypeReference<List<Rating>>() {})
 				.block();
 		
-		return userRating.getUserRating().stream().map(rating -> {
+		return ratingList.stream().map(rating -> {
 			Movie movie = webClientBuilder.build()
 				.get()
-				.uri("http://localhost:8082/movies/" + rating.getMovieId())
+				.uri("http://localhost:8082/movie/" + rating.getMovieId())
 				.retrieve()
 				.bodyToMono(Movie.class)
 				.block();
-			return new CatalogItem(movie.getName(), "Description", rating.getRating());
+			return new CatalogItem(movie.getName(), rating.getRating());
 		}).collect(Collectors.toList());
 	}
 }
--- src/main/java/com/dtw/movieCatalog/model/CatalogItem.java
+++ src/main/java/com/dtw/movieCatalog/model/CatalogItem.java
@@ -1,21 +1,13 @@
 package com.dtw.movieCatalog.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor @NoArgsConstructor
 public class CatalogItem {
 
 	private String name;
-	private String description;
 	private int rating;
-	
-	public CatalogItem(String name, String description, int rating) {
-		this.name = name;
-		this.description = description;
-		this.rating = rating;
-	}
-
-	public String getName() {return name;}
-	public void setName(String name) {this.name = name;}
-	public String getDescription() {return description;}
-	public void setDescription(String description) {this.description = description;}
-	public int getRating() {return rating;}
-	public void setRating(int rating) {this.rating = rating;}
 }
