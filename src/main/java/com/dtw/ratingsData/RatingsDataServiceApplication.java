9398b2d3b1a3c59dc74bb6203f4793572e5687b4
David de Castro Hernandez <itsdavidch@gmail.com>
2021-05-04 18:43:46 +0200

    Added eureka support

--- src/main/java/com/dtw/ratingsData/RatingsDataServiceApplication.java
+++ src/main/java/com/dtw/ratingsData/RatingsDataServiceApplication.java
@@ -2,7 +2,9 @@ package com.dtw.ratingsData;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+@EnableEurekaClient
 @SpringBootApplication
 public class RatingsDataServiceApplication {
 
