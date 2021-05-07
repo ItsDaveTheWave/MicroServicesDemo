4a1c96c502a3995f6af40a48fa8af80ce2a00324
David de Castro Hernandez <itsdavidch@gmail.com>
2021-05-07 16:50:34 +0200

    Added timeout of 10 seconds

--- src/main/java/com/dtw/movieCatalog/MovieCatalogServiceApplication.java
+++ src/main/java/com/dtw/movieCatalog/MovieCatalogServiceApplication.java
@@ -1,11 +1,16 @@
 package com.dtw.movieCatalog;
 
+import java.time.Duration;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.client.reactive.ReactorClientHttpConnector;
 import org.springframework.web.reactive.function.client.WebClient;
+
+import reactor.netty.http.client.HttpClient;
 
 @EnableEurekaClient
 @SpringBootApplication
@@ -18,6 +23,8 @@ public class MovieCatalogServiceApplication {
 	@Bean
 	@LoadBalanced
 	public WebClient.Builder webClientBuilder() {
-		return WebClient.builder();
+		return WebClient.builder()
+				.clientConnector(new ReactorClientHttpConnector(
+						HttpClient.create().responseTimeout(Duration.ofMillis(10000))));
 	}
 }
