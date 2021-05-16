892f9487760c4fd1b935c61d2afcde2c4ced2217
David de Castro Hernandez <itsdavidch@gmail.com>
2021-05-16 15:14:23 +0200

    Initial commit

---
+++ src/main/java/com/dtw/oAuth2Server/OAuth2ServerApplication.java
@@ -0,0 +1,13 @@
+package com.dtw.oAuth2Server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OAuth2ServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OAuth2ServerApplication.class, args);
+	}
+
+}
