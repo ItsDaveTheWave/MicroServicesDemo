a41f50eb2c68d9cc5cc3115a2cc0e7bc2fd0d988
David de Castro Hernandez <itsdavidch@gmail.com>
2021-06-02 07:02:25 +0200

    Added creation method

--- src/main/java/com/dtw/ratingsData/controller/RatingController.java
+++ src/main/java/com/dtw/ratingsData/controller/RatingController.java
@@ -8,6 +8,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -32,5 +34,10 @@ public class RatingController {
 	@GetMapping
 	public ResponseEntity<List<Rating>> getAll() {
 		return ResponseEntity.ok(ratingRepo.findAll());
+	}
+	
+	@PostMapping
+	public ResponseEntity<Rating> create(@RequestBody Rating rating) {
+		return new ResponseEntity<Rating>(ratingRepo.save(rating), HttpStatus.CREATED);
 	}
 }
