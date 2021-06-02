c932d53a577b179f5abdf4e2e201c463ec047b73
David de Castro Hernandez <itsdavidch@gmail.com>
2021-06-02 06:42:45 +0200

    Implemented oauth2 security

--- src/main/java/com/dtw/movieData/MovieDataServiceApplication.java
+++ src/main/java/com/dtw/movieData/MovieDataServiceApplication.java
@@ -7,11 +7,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.client.reactive.ReactorClientHttpConnector;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.web.reactive.function.client.WebClient;
 
 import reactor.netty.http.client.HttpClient;
 
 @EnableEurekaClient
+@EnableResourceServer
 @SpringBootApplication
 public class MovieDataServiceApplication {
 
