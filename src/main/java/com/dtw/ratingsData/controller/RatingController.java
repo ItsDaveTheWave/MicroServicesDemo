27949fdbc749bc744cd649b0607b47daac6169f7
David de Castro Hernandez <itsdavidch@gmail.com>
2021-06-06 08:39:28 +0200

    Allowed cross origin from angular web client(localhost:4200)

--- src/main/java/com/dtw/ratingsData/controller/RatingController.java
+++ src/main/java/com/dtw/ratingsData/controller/RatingController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -19,6 +20,7 @@ import com.dtw.ratingsData.repo.RatingRepo;
 
 @RestController
 @RequestMapping("/rating")
+@CrossOrigin("http://localhost:4200")
 public class RatingController {
 
 	@Autowired
